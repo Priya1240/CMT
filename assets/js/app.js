dbe6be9c600c5a945bc2ab1703a2bb3756de8ecf
Priya <piyar.pi@gmail.com>
2018-02-05 20:04:01 -0500

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
       $('.carousel').carousel();
+      autoplay();
     });
+
+
+function autoplay() {
+   $('.carousel').carousel('next');
+   setTimeout(autoplay, 2000);
+}
